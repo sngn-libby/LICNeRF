fa8fe5957eb2fb6529f741d4f6631b4782560d1b
clairpastel@gmail.com <clairpastel@gmail.com>
2023-09-12 23:01:32 +0900

    Applying Nerfusion

---
+++ src/compressproj/elic/modules/transform/__init__.py
@@ -0,0 +1,4 @@
+from src.compressproj.elic.modules.transform.analysis import *
+from src.compressproj.elic.modules.transform.synthesis import *
+from src.compressproj.elic.modules.transform.context import *
+from src.compressproj.elic.modules.transform.entropy import *
---
+++ src/compressproj/version.py
@@ -0,0 +1,2 @@
+__version__ = "1.2.0.dev0"
+git_version = "61292e5d96ed3bb5ed48f5512f3f0bd4f9f8e198"
